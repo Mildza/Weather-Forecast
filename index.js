0bd8bc140b2d1665cd49ad6d38c4b707a9eaf860
Milan Vukovic <mildzan@gmail.com>
2020-03-06 18:11:09 +0100

    heroku

--- index.js
+++ index.js
@@ -12,4 +12,12 @@ app.use(bodyParser.json());
 app.use("/auth", auth);
 app.use("/yahoo", yahoo);
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("frontend/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
+}
+
 app.listen(process.env.PORT || 3001);
